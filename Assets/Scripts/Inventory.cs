a2532e9598f7d5b5adaa9067703f33e870e77230
MarcosGorriaran <marcos.gorriaran.7e7@itb.cat>
2025-01-14 15:57:25 +0100

    Implemeting dropable weapons.

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -1,8 +1,20 @@
+using System.Linq;
 using UnityEngine;
 
 public class Inventory : MonoBehaviour
 {
-    public Slot[] slots;
-
+    [SerializeField]
+    private WeaponSO[] slots;
     
+    public WeaponSO ChangeWeapon(uint slotIndex)
+    {
+        return slots[slotIndex];
+    }
+    public void AddWeapon(WeaponSO weapon)
+    {
+        int nullIndex = slots.Select((value,index)=>new { value, index })
+                        .Where(x => x.value == null)
+                        .Select(x => x.index).FirstOrDefault();
+        slots[nullIndex] = weapon;
+    }
 }
