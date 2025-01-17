c42025da82f3f4398d34b20bb9c67b2f92aca6b8
MarcosGorriaran <marcos.gorriaran.7e7@itb.cat>
2025-01-17 17:45:00 +0100

    Implemented HPBar

---
+++ Assets/Scripts/UI/HPBar.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Slider))]
+public class HPBar : MonoBehaviour
+{
+    public HPManager trackedHp;
+    private Slider _slider;
+
+    private void Awake()
+    {
+        trackedHp.onHPChange += UpdateBar;
+        _slider = GetComponent<Slider>();
+        _slider.maxValue = trackedHp.GetMaxHp();
+        _slider.minValue = 0;
+        _slider.value = trackedHp.GetHp();
+    }
+    private void UpdateBar(float damage)
+    {
+        _slider.value = trackedHp.GetHp();
+    }
+}
