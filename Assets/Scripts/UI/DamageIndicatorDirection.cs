21cb22b0fe08e36454d5b03a921a5096f30323a9
MarcosGorriaran <marcos.gorriaran.7e7@itb.cat>
2025-01-27 17:45:45 +0100

    Fixing issues with damagedirection

--- Assets/Scripts/UI/DamageIndicatorDirection.cs
+++ Assets/Scripts/UI/DamageIndicatorDirection.cs
@@ -8,6 +8,7 @@ public class DamageIndicatorDirection : DamageIndicator
     [SerializeField]
     Transform _charOrigin;
     float _offsetFromCenter;
+    float _offsetRotation;
     Coroutine _countdownCoroutine;
 
     protected override void Awake()
@@ -15,7 +16,8 @@ public class DamageIndicatorDirection : DamageIndicator
         base.Awake();
         _charHP.onHPChangeBy += OnHarm;
         _directionIndicator.enabled = false;
-        _offsetFromCenter = Vector2.Distance(_directionIndicator.transform.position, Vector2.zero);
+        _offsetFromCenter = Vector2.Distance(_directionIndicator.transform.localPosition, Vector2.zero);
+        _offsetRotation = _directionIndicator.transform.localEulerAngles.z;
     }
 
     private void OnHarm(float damage,GameObject source)
@@ -31,7 +33,7 @@ public class DamageIndicatorDirection : DamageIndicator
         Vector2 direction = (source.transform.position - _charOrigin.position).normalized;
         float angle = Mathf.Atan2(direction.y, direction.x);
 
-        _directionIndicator.rectTransform.localRotation = Quaternion.Euler(0, 0, angle * 180f / Mathf.PI - 90f);
+        _directionIndicator.rectTransform.localRotation = Quaternion.Euler(0, 0, (angle * 360f / Mathf.PI - 90f)+_offsetRotation);
         _directionIndicator.rectTransform.localPosition = direction * _offsetFromCenter;
 
         _countdownCoroutine = StartCoroutine(DesactivateIndicatorOn(_directionIndicator));
