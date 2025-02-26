ed93107f324eb91cc3ba94c2576e57c1f5c74bae
MarcosGorriaran <marcos.gorriaran.7e7@itb.cat>
2025-02-26 17:50:04 +0100

    Developing the scrolable lists and the manager.

---
+++ Assets/Scripts/PrefabsOfInputManager/ProfilePicker/PickManager.cs
@@ -0,0 +1,98 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+public abstract class PickManager : MonoBehaviour
+{
+    private int playersReady = 0;
+    private int maxPlayers;
+    private PlayerInputManager playerInputManager;
+    private static PickManager _instance;
+
+    public static PickManager Instance
+    {
+        get { return _instance; }
+
+    }
+    protected virtual void Awake()
+    {
+        if (Instance == null)
+        {
+            _instance = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+    protected virtual void Start()
+    {
+        maxPlayers = PlayerPrefs.GetInt("SelectedNumber");
+        playerInputManager = FindAnyObjectByType<PlayerInputManager>();
+
+        if (playerInputManager != null)
+        {
+            Debug.Log("PlayerInputManager encontrado.");
+        }
+        else
+        {
+            Debug.LogError("No se encontró un PlayerInputManager.");
+        }
+    }
+
+    public void OnPlayerJoined(PlayerInput playerInput)
+    {
+        // Comprueba si hay espacio para más jugadores
+        if (playerInputManager.playerCount > maxPlayers)
+        {
+            Debug.Log($"Se alcanzó el límite de jugadores ({maxPlayers}). Desconectando jugador {playerInput.playerIndex}.");
+            Destroy(playerInput.gameObject); // Elimina el jugador adicional
+        }
+        else
+        {
+            Debug.Log($"Jugador {playerInput.playerIndex} unido. Total: {playerInputManager.playerCount}/{maxPlayers}");
+        }
+    }
+
+    public void SumePlayerReady(int playerIndex)
+    {
+        if (playersReady < maxPlayers)
+        {
+            playersReady++;
+            CheckPlayersReady();
+        }
+        else
+        {
+            Debug.Log("No se puede sumar más jugadores listos. Máximo permitido alcanzado.");
+        }
+    }
+
+    public void RestPlayerReady(int playerIndex)
+    {
+        if (playersReady > 0)
+        {
+            playersReady--;
+            CheckPlayersReady();
+        }
+        else
+        {
+            Debug.Log("No hay jugadores listos para quitar.");
+        }
+    }
+
+    public void CheckPlayersReady()
+    {
+        if (playersReady == maxPlayers)
+        {
+            NextScene();
+        }
+        else
+        {
+            Debug.Log($"Aún faltan jugadores para estar listos. Jugadores listos: {playersReady}");
+        }
+    }
+
+    protected abstract void NextScene();
+}
